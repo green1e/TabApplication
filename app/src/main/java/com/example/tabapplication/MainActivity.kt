967d6aab88bdc0e6017d18227570b8deb2f24054
manish061099 <manishm@apna.co>
2021-11-25 20:57:44 +0530

    migrated to viewpager 2

--- app/src/main/java/com/example/tabapplication/MainActivity.kt
+++ app/src/main/java/com/example/tabapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tabapplication
 
+import android.app.ActionBar
+import android.content.res.Resources
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,13 +10,25 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import com.example.tabapplication.ui.main.SectionsPagerAdapter
 import com.example.tabapplication.databinding.ActivityMainBinding
 import com.google.android.material.appbar.AppBarLayout
+import android.util.DisplayMetrics
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private val TAB_TITLES = arrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +36,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getString(TAB_TITLES[position])
+        }.attach()
 
-        binding.btnDisableScroll.setOnClickListener { view ->
+
+        // code to prevent app bar scrolling beyond a point
+
+        //val width1 = Resources.getSystem().displayMetrics.widthPixels
+        //val height1 = Resources.getSystem().displayMetrics.heightPixels
+        //binding.title.minimumHeight = height - 500
+
+        binding.btnDisableScroll.setOnClickListener {
             val params = binding.title.layoutParams as AppBarLayout.LayoutParams
             params.scrollFlags = AppBarLayout.LayoutParams.SCROLL_FLAG_NO_SCROLL
             binding.title.layoutParams = params
--- app/src/main/java/com/example/tabapplication/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/tabapplication/ui/main/SectionsPagerAdapter.kt
@@ -2,37 +2,27 @@ package com.example.tabapplication.ui.main
 
 import android.content.Context
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.tabapplication.R
-
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val fa: FragmentActivity) :
+    FragmentStateAdapter(fa) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> NestedScrollFragment.newInstance(1)
             else -> PlaceholderFragment.newInstance(2)
         }
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
     }
 }
